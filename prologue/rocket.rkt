879bf4725a54ef5e470aac3a55411885cb34154e
haohiao <haohiao@qq.com>
2024-10-13 12:44:12 +0800

    Landing a rocket (version 1): simple code

---
+++ prologue/rocket.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname rocket) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image) 
+(require 2htdp/universe)
+
+(define (picture-of-rocket height)
+  (place-image
+   (bitmap "./resources/images/rocket.png")
+   50
+   height
+   (empty-scene 100 60)))
+
+
+(animate picture-of-rocket)

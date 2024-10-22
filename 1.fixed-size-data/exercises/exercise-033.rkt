d45059edd2b4649310773403ceca8e424914e264
haohiao <haohiao@qq.com>
2024-10-22 23:38:48 +0800

    add exercise-033

---
+++ 1.fixed-size-data/exercises/exercise-033.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-033) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; 千年虫问题
+; WHAT
+; 只使用两位二进制表示年份，导致年份从1999年(99)流逝到2000年(00)时出现的BUG。
+
+; 0 0
+;   >
+;  U
